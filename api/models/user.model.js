85850f81192541e9fed694490d12239f60296fa3
BillsOnKen <110493939+BillsOnKen@users.noreply.github.com>
2023-11-08 13:29:11 +0100

    create a middleware and function

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-    }
-    
-     email: {
+    },
+    email: {
         type: String,
         required: true,
         unique: true,
-    }
-     
+    },
     password: {
         type: String,
         required: true,
