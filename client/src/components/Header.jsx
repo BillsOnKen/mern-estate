5701accc20cf88ecc79a15d1d78dd152eb844f5e
BillsOnKen <110493939+BillsOnKen@users.noreply.github.com>
2023-11-28 10:20:39 +0100

    complete sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,6 +27,9 @@ export default function Header() {
           <Link to='/sign-in'>
             <li className='text-slate-700 hover:underline'> Sign In </li>
           </Link>
+          <Link to='/sign-up'>
+            <li className='text-slate-700 hover:underline'> Sign Up </li>
+          </Link>
         </ul>
         </div>
     </header>
